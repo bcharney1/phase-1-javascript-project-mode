a3b240d76b50d70b30010b49be7c17d72f800f85
Boris Charney <boris.charney@gmail.com>
2022-11-08 00:00:48 -0800

    Create skeleton for saving cards to list

--- script.js
+++ script.js
@@ -4,17 +4,28 @@ const searchResults = []
 
 function searchCardsByName(cardName) {
   fetch(buildSearchURL(cardName))
-  .then(response => response.json())
-  .then(data => {
-    console.log("DATA", data)
-    clearSearchResults()
-    renderSearchResults(data.data)
+    .then(response => response.json())
+    .then(data => {
+      console.log("DATA", data)
+      clearSearchResults()
+      searchResults = data.data
+      renderSearchResults(data.data)
+    })
+}
+
+function getSearchedCardById(cardId) {
+  searchResults.forEach((card) => {
+    if (card.id === cardId) {
+      return card
+    }
   })
+
+  return null
 }
 
 function clearSearchResults() {
-    const cardList = document.querySelector('#cardsearch-container')
-    cardList.innerHTML = ""
+  const cardList = document.querySelector('#cardsearch-container')
+  cardList.innerHTML = ""
 }
 
 function buildSearchURL(cardName) {
@@ -29,13 +40,14 @@ function renderSearchResult(cardObj) {
   const cardList = document.querySelector('#cardsearch-container')
 
   const cardContainer = document.createElement("li")
+  cardContainer.id = cardObj.id
   // click handler attach
   cardContainer.addEventListener("click", function (e) {
-    // save the card to the list
+    saveCard(e)
   })
   // hover handler attach
   cardContainer.addEventListener("hover", function (e) {
-    // show card sets
+    // show card sets or prices or whatever
   })
   const cardImage = document.createElement("img")
   cardImage.src = cardObj.image_uris.small
@@ -48,8 +60,11 @@ function displayCardSets(cardObj) {
 
 }
 
-function saveCard(cardObj) {
-
+function saveCard(e) {
+  // get card id from e.target.id
+  // pass this id to getSearchedCardById, storing result in a variable
+  // add the value in the variable to savedCards
+  // render the list of saved cards to the page (cardstorage-container)
 }
 
 function renderSavedCards() {
@@ -61,13 +76,11 @@ function renderSavedCard(cardObj) {
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-    const cardSearchForm = document.querySelector("#cardsearch-form")
-    cardSearchForm.addEventListener("submit", (e) => {
-        e.preventDefault()
-        const searchValue = e.target.elements[0].value
-        searchCardsByName(searchValue)
-    })
-  // attach submit handler
-
+  const cardSearchForm = document.querySelector("#cardsearch-form")
+  cardSearchForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+    const searchValue = e.target.elements[0].value
+    searchCardsByName(searchValue)
+  })
 })
 
