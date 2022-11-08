d1bb86a6fa6accf8e5d98d5267ea3f6b90e357f0
Boris Charney <boris.charney@gmail.com>
2022-11-07 23:12:02 -0800

    Create initial js structure

---
+++ script.js
@@ -0,0 +1,43 @@
+const scryfallUrl = 'https://api.scryfall.com/cards/search'
+const savedCards = []
+const searchResults = []
+
+function searchForCard(cardName) {
+  // lblkj
+}
+
+function buildSearchURL(cardName) {
+  // asldjkfalskdjf
+}
+
+function renderSearchResults(results) {
+  // for each result...
+}
+
+function renderSearchResult(cardObj) {
+  // click handler attach
+  // hover handler attach
+}
+
+function displayCardSets(cardObj) {
+
+}
+
+function saveCard(cardObj) {
+
+}
+
+function renderSavedCards() {
+  // for each saved card...
+}
+
+function renderSavedCard(cardObj) {
+
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+
+  // attach submit handler
+
+})
+
