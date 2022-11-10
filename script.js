69d44a87607456dcea2fbe70bc45067bdbcdc4d0
Boris Charney <boris.charney@gmail.com>
2022-11-09 18:55:23 -0800

    Set up functions to save cards on page

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 const scryfallUrl = 'https://api.scryfall.com/cards/search?format=json&include_extras=false&include_multilingual=false&include_variations=false&'
-const savedCards = []
-const searchResults = []
+let savedCards = []
+let searchResults = []
 
 function searchCardsByName(cardName) {
   fetch(buildSearchURL(cardName))
     .then(response => response.json())
     .then(data => {
-      console.log("DATA", data)
       clearSearchResults()
       searchResults = data.data
       renderSearchResults(data.data)
@@ -14,17 +13,24 @@ function searchCardsByName(cardName) {
 }
 
 function getSearchedCardById(cardId) {
+  let grabbedCard = null
+
   searchResults.forEach((card) => {
     if (card.id === cardId) {
-      return card
+      grabbedCard = card
     }
   })
 
-  return null
+  return grabbedCard
 }
 
 function clearSearchResults() {
   const cardList = document.querySelector('#cardsearch-container')
+  cardList.innerHTML = ""
+}
+
+function clearSavedCards() {
+  const cardList = document.querySelector('#saved-cards')
   cardList.innerHTML = ""
 }
 
@@ -51,6 +57,7 @@ function renderSearchResult(cardObj) {
   })
   const cardImage = document.createElement("img")
   cardImage.src = cardObj.image_uris.small
+  cardImage.id = cardObj.id
 
   cardContainer.appendChild(cardImage)
   cardList.appendChild(cardContainer)
@@ -62,17 +69,47 @@ function displayCardSets(cardObj) {
 
 function saveCard(e) {
   // get card id from e.target.id
-  // pass this id to getSearchedCardById, storing result in a variable
-  // add the value in the variable to savedCards
+  const cardId = e.target.id
+  const isAlreadySaved = savedCards.some((card) => card.id === cardId)
+
+  if (isAlreadySaved) {
+    savedCards = savedCards.filter((card) => card.id !== cardId)
+  } else {
+    // pass this id to getSearchedCardById, storing result in a variable
+    const searchedCard = getSearchedCardById(cardId)
+    // add the value in the variable to savedCards
+    savedCards = [...savedCards, searchedCard]
+  }
+
   // render the list of saved cards to the page (cardstorage-container)
+  renderSavedCards()
 }
 
 function renderSavedCards() {
   // for each saved card...
+  clearSavedCards()
+  savedCards.forEach((savedCard) => {renderSavedCard(savedCard)})
 }
 
 function renderSavedCard(cardObj) {
+  const cardList = document.querySelector('#saved-cards')
 
+  const cardContainer = document.createElement("li")
+  cardContainer.id = cardObj.id
+  // click handler attach
+  cardContainer.addEventListener("click", function (e) {
+    saveCard(e)
+  })
+  // hover handler attach
+  cardContainer.addEventListener("hover", function (e) {
+    // show card sets or prices or whatever
+  })
+  const cardImage = document.createElement("img")
+  cardImage.src = cardObj.image_uris.small
+  cardImage.id = cardObj.id
+
+  cardContainer.appendChild(cardImage)
+  cardList.appendChild(cardContainer)
 }
 
 document.addEventListener("DOMContentLoaded", function () {
